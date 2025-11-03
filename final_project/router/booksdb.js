07065f513adc0eeb78d22f7a928a960d4c6a1640
bvmohr <bvmohr2@gmail.com>
2025-11-03 00:17:16 -0500

    completed task 3

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "ISBN": "1234567891" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "ISBN": "1235567654"},
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "ISBN": "8761234578"},
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "ISBN": "7985371254"},
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "ISBN": "9472837846" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "ISBN": "8472639813" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "ISBN": "8391897546" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "ISBN": "1237645198" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "ISBN": "4328751987" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "ISBN": "9019678365" }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,40 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const ISBN = req.params.isbn;
+
+    for (const bookID in books) {
+        const book = books[bookID];
+        if (book.ISBN === ISBN) {
+            return res.send(JSON.stringify(book, null, 4));
+        }
+    }
+    return res.send(`The book with the ISBN, ${ISBN}, cannot be found!`);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    matchingBooks = [];
+
+    for (const bookID in books) {
+        const book = books[bookID];
+        if (book.author === author) {
+            matchingBooks.push(book);
+        }
+    }
+    
+    if (matchingBooks.length > 0) {
+        return res.send(JSON.stringify(matchingBooks, null, 4));
+    } else {
+        return res.send(`The book with the author, ${author}, cannot be found!`)
+    }
 });
 
 // Get all books based on title
